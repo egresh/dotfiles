c723fb87356bf7fd84e7de2a01386af723c808aa
Earl Gresh <earl.gresh@gmail.com>
2022-10-10 13:43:00 -0700

    Adjusted yabai's bsp settings'

--- yabai/yabai/scripts/bsp.sh
+++ yabai/yabai/scripts/bsp.sh
@@ -2,8 +2,8 @@
 export PATH=/opt/homebrew/bin:$PATH
 
 yabai -m space  --layout bsp
-yabai -m config top_padding    10
+yabai -m config top_padding    8
 yabai -m config bottom_padding 20
 yabai -m config left_padding   20
 yabai -m config right_padding  20
-yabai -m config window_gap     10
+yabai -m config window_gap     20
